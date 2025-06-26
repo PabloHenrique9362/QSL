9d971ae3dddf7f43ef101b68eea8b0dffe84a7e3
Pablo Henrique <Pabloh1ff@gmail.com>
2025-06-25 21:25:09 -0300

    Add files via upload

---
+++ matricula.sql
@@ -0,0 +1,14 @@
+CREATE TABLE pessoas (
+id_aluno VARCHAR(100),
+curso VARCHAR(100),
+data__matricula DATE
+);
+-- Inserçao de dados na tabela--
+INSERT INTO pessoas (id_aluno, nome_aluno, curso, data_matricula)
+VALUES
+	(123, 'João Silva', 'Python', '2025-01-10'),
+    (123, 'João Silva', 'Java', '2025-02-05'),
+    (254, 'Maria Souza', 'Excel', '2025-03-12'),
+    (397, 'Ana lima', 'Python', '2025-01-15'),
+    (397, 'Ana lima', 'Java Script', '2025-02-01'),
+    (397, 'Ana lima', 'HTML', '2025-03-10');
